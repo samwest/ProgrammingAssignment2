e01e001accb131195e489d2cd9df2bd83703babb
samwest <swest152@gmail.com>
2014-12-17 19:53:47 -0500

    final submissio

--- cachematrix.R
+++ cachematrix.R
@@ -1,82 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Course R Programming
+## Assignment 3
+## Author Sam West
+## Purpose - write function to take a matrix and store it as an object which 
+##           can stored to later cache the inverse of the matrix
+##
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  #makeVector <- function(x = numeric()) {    #geginning of function
+## Create object to store a matrix and it's inverse
+
+makeCacheMatrix <- function(inputMatrix = matrix()) {
   inverseMatrix = NULL
-  set <- function(y) {
-    x <<- y
+  set <- function(initInput) {
+    inputMatrix <<- initInput
     inverseMatrix <<- NULL
   }
-  #Undeveloped
-  get <- function() x
-  setInverse <- function(solve) m <<- solve
-  getInverse <- function() m
+  get <- function() inputMatrix
+  setInverse <- function(solve) inverseMatrix <<- solve
+  getInverse <- function() inverseMatrix
   list(set = set, get = get,
        setInverse = setInverse,
-       getInverse = getInverse)
-  
+       getInverse = getInverse) 
 
 }
 
 
-## Write a short comment describing this function
+## Takes a matrix object and returns it's inverse. If the matrix has already been cached, then
+## this returns a message that the data has been cached and the inverse of the original matrix.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrixObj, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-  inverseMatrix <- x$getInverse()
+  inverseMatrix <- matrixObj$getInverse()
   if(!is.null(inverseMatrix)) {
     message("getting cached data")
     return(inverseMatrix)
   }
-  data <- x$get()
+  data <- matrixObj$get()
   inverseMatrix <- solve(data,...) # ?solve parameters?
-  x$setmean(insertMatrix)
-  insertMatrix
-  
-  #            m <- x$getmean()
-  #if(!is.null(m)) {
-  #  message("getting cached data")
-  #  return(m)
-  #}
-  #data <- x$get()
-  #m <- mean(data, ...)
-  #x$setmean(m)
-  #m
-  
+  matrixObj$setInverse(inverseMatrix, ...)
+  inverseMatrix
 }  
+
+  
+
   
   
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set<- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-}
-  
-myTestFun <- function() {
-  print ("hellow world!")
-}
-  
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-  
-}
+
